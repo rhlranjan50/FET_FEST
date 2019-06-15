71988680448b1b22156526839567406ba9ce521f
NageshAdhe <nageshadhe@hotmail.com>
2019-06-15 13:35:57 +0530

    other changes

---
+++ frontend/src/app/pages/blood-donation-events/blood-donation-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blood-donation-events',
+  templateUrl: './blood-donation-events.component.html',
+  styleUrls: ['./blood-donation-events.component.css']
+})
+export class BloodDonationEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
