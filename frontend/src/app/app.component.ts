9365149c5b4d584f14c5e7bef158f292fa0a3f06
rupeshpatil <rupeshpatil.461@gmail.com>
2019-06-15 16:37:05 +0530

    Handling maps style

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'digital-blood-bank';
+
+  constructor(
+    private router:Router
+  ){  }
+
+  ngOnInit() {
+    let userData = localStorage.getItem('digiBloodUser');
+    if(userData){
+        this.router.navigate(['/home']);
+    } else {
+      this.router.navigate(['/userRegister']);
+    }
+  }
 }
